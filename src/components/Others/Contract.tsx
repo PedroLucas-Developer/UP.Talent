65d2b1fe930976366a503a9f42148a17b2d19083
Nuru-pedro <73931509+PedroLucas-Developer@users.noreply.github.com>
2024-04-21 16:37:27 -0300

    update detalhes

--- src/components/Others/Contract.tsx
+++ src/components/Others/Contract.tsx
@@ -10,7 +10,7 @@ export default function Contract() {
     <div  className="relative flex justify-center flex-col">
       <div className='pt-[300px] relative mx-auto max-[370px]:pt-[180px]'>
         <div id='Contract' className='relative max-[1250px]:pt-14 max-[400px]:pt-0 mx-auto max-[600px]:bg-[#df887c]'>
-          <p className="text-[60px]  max-[800px]:w-10/12 mx-auto font-bold leading-[50px] text-[#291234] text-center mb-[10px] max-[800px]:text-[55px] max-[400px]:text-[45px]">
+          <p className="text-[60px] max-[800px]:w-10/12 mx-auto font-bold leading-[50px] text-[#291234] text-center mb-[10px] max-[800px]:text-[55px] max-[400px]:text-[45px]">
             Modelos de contratação
           </p>
           <div className='mt-6 flex relative justify-center'>
@@ -19,35 +19,35 @@ export default function Contract() {
         </div>
       </div>
       <div className='max-[1250px]:bg-[#df887c] pb-[150px]'>
-        <div className='pt-[220px] relative mx-auto w-11/12 max-w-[1270px] max-[800px]:pt-[100px]'>
+        <div className='pt-[220px] relative mx-auto w-11/12 max-w-[1270px] max-[1250px]:pt-[50px]'>
           <div className='flex w-full flex-row justify-between max-[1250px]:flex-col'>
-            <div className='flex flex-row justify-between w-[812px] max-[1250px]:w-full max-[800px]:flex-col'>
-              <div className="w-[272px] mb-auto flex flex-col justify-center max-[1250px]:w-1/2 max-[800px]:w-full">
+            <div className='flex flex-row justify-between w-[812px] max-[1250px]:w-full max-[600px]:flex-col'>
+              <div className="w-[272px] mb-auto flex flex-col justify-center max-[1250px]:w-1/2 max-[600px]:w-full">
                 <div className="flex mx-auto">
-                  <div className='h-[280px]'>
+                  <div className='h-[280px] max-[1250px]:h-[250px]'>
                     <Image loading="lazy" src={Caledar} alt="" />
                   </div>
                 </div>
                 <div className="flex flex-col justify-center mx-auto">
-                  <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center">
+                  <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center max-[1250px]:text-[35px]">
                     Job Pontual
                   </p>
-                  <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center">
+                  <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center max-[1250px]:mt-4 max-[1250px]:text-[25px]">
                     Contrato pot vaga
                   </p>
                 </div>
               </div>
-              <div className="w-[353px] mb-auto flex flex-col justify-center max-[1250px]:w-1/2 max-[800px]:w-full max-[800px]:pt-10">
+              <div className="w-[353px] mb-auto flex flex-col justify-center max-[1250px]:w-1/2 max-[600px]:w-full max-[600px]:pt-10">
                 <div className="flex mx-auto">
-                  <div className='h-[280px]'>
+                  <div className='h-[280px] max-[1250px]:h-[250px]'>
                     <Image loading="lazy" src={Fee} alt="" />
                   </div>
                 </div>
                 <div className="flex flex-col justify-center mx-auto">
-                  <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center">
+                  <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center max-[1250px]:text-[35px]">
                     Fee por período
                   </p>
-                  <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center">
+                  <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center max-[1250px]:mt-4 max-[1250px]:text-[25px]">
                     Com a quantidade de
                     <br />
                     vagas a combinar
@@ -55,17 +55,17 @@ export default function Contract() {
                 </div>
               </div>
             </div>
-            <div className="w-[353px] mb-auto flex flex-col justify-center max-[1250px]:pt-10 max-[1250px]:w-full">
+            <div className="w-[353px] ml-[50px] mb-auto flex flex-col justify-center  max-[1250px]:ml-0 max-[1250px]:pt-10 max-[1250px]:w-full">
               <div className="flex mx-auto">
-                <div className='pt-4 h-[280px]'>
+                <div className='pt-4 h-[280px] max-[1250px]:h-[250px]'>
                   <Image loading="lazy" src={Hub} alt="" />
                 </div>
               </div>
               <div className="flex flex-col justify-center mx-auto">
-                <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center">
+                <p className="text-[40px] text-[#FFFFFF] leading-[40px] font-bold text-center max-[1250px]:text-[35px]">
                   Hub dedicado
                 </p>
-                <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center">
+                <p className="text-[30px] mt-12 text-[#FFFFFF] leading-[40px] text-center max-[1250px]:mt-4 max-[1250px]:text-[25px]">
                   para atender o RH da
                   <br />
                   empresa
--- src/components/service/cards.tsx
+++ src/components/service/cards.tsx
@@ -10,10 +10,10 @@ export default function Cards() {
   return (
     <div className="flex flex-row relative">
       <div className='w-11/12 flex flex-row mx-auto max-w-[1400px] mt-[140px] max-[1250px]:mt-[50px] max-[1550px]:mt-[200px] max-[1550px]:flex-col'>
-        <div className='flex flex-row max-[1250px]:flex-col mx-auto max-[1250px]:w-full'>
-          <div className='flex flex-row max-[550px]:flex-col max-[1250px]:w-full max-[1250px]:justify-between'>
-            <div className="w-[201px] flex flex-col justify-center mx-auto">
-              <div className="flex mx-auto h-[160px] ">
+        <div className='flex flex-row max-[910px]:flex-col mx-auto max-[910px]:w-full'>
+          <div className='flex flex-row max-[550px]:flex-col max-[910px]:w-full max-[910px]:justify-between'>
+            <div className="w-[201px] mb-auto flex flex-col justify-center mx-auto">
+              <div className="flex mx-auto h-[160px] max-[1250px]:h-[120px]">
                 <div>
                   <Image loading="lazy" src={Find} alt="" />
                 </div>
@@ -26,8 +26,8 @@ export default function Cards() {
                 </p>
               </div>
             </div>
-            <div className="w-[209px] ml-[95px] max-[550px]:mt-8  flex flex-col max-[1250px]:mx-auto max-[550px]:mx-auto justify-center">
-              <div className="flex mx-auto h-[160px]">
+            <div className="w-[209px] mb-auto ml-[95px] max-[550px]:mt-8  flex flex-col max-[910px]:mx-auto max-[550px]:mx-auto justify-center">
+              <div className="flex mx-auto h-[160px] max-[1250px]:h-[120px]">
                 <div className='pt-2 max-[1550px]:pt-0'>
                   <Image loading="lazy" src={Interview} alt="" />
                 </div>
@@ -41,8 +41,8 @@ export default function Cards() {
               </div>
             </div>
           </div>
-          <div className="w-[252px] ml-[70px] flex flex-col max-[1250px]:mx-auto max-[1250px]:mt-8 justify-center max-[600px]:w-full">
-            <div className="flex mx-auto h-[160px]">
+          <div className="w-[252px] mb-auto ml-[70px] flex flex-col max-[910px]:mx-auto max-[910px]:mt-8 justify-center max-[600px]:w-full">
+            <div className="flex mx-auto h-[160px] max-[1250px]:h-[120px]">
               <div className='mt-3 max-[1550px]:mt-0'>
                 <Image loading="lazy" src={Meets} alt="" />
               </div>
@@ -58,9 +58,9 @@ export default function Cards() {
             </div>
           </div>
         </div>
-        <div className='flex flex-row max-[550px]:flex-col max-[1550px]:mt-5 mx-auto max-[1250px]:w-full max-[1250px]:justify-between'>
-          <div className="w-[260px] ml-[85px] max-[1550px]:ml-0 flex flex-col max-[550px]:mx-auto justify-center max-[1250px]:w-1/2 max-[600px]:w-full">
-            <div className="flex mx-auto h-[160px]">
+        <div className='flex flex-row max-[550px]:flex-col max-[1550px]:mt-5 mx-auto max-[910px]:w-full max-[910px]:justify-between'>
+          <div className="w-[260px] mb-auto ml-[85px] max-[1550px]:ml-0 flex flex-col max-[550px]:mx-auto justify-center max-[910px]:w-1/2 max-[600px]:w-full">
+            <div className="flex mx-auto h-[160px] max-[1250px]:h-[120px]">
               <div className='mr-20 mt-7 max-[1550px]:mr-0 max-[1550px]:mt-0'>
                 <Image loading="lazy" src={Gift} alt="" />
               </div>
@@ -75,8 +75,8 @@ export default function Cards() {
               </p>
             </div>
           </div>
-          <div className="w-[212px] ml-[55px] max-[1250px]:mr-0 max-[550px]:mx-auto max-[1550px]:mx-auto max-[1250px]:w-1/2 max-[550px]:mt-8 flex flex-col justify-center max-[600px]:w-full">
-            <div className="flex mx-auto h-[160px]">
+          <div className="w-[212px] mb-auto ml-[55px] max-[910px]:mr-0 max-[550px]:mx-auto max-[1550px]:mx-auto max-[910px]:w-1/2 max-[550px]:mt-8 flex flex-col justify-center max-[600px]:w-full">
+            <div className="flex mx-auto h-[160px] max-[1250px]:h-[120px]">
               <div className='pt-10 max-[1550px]:pt-0'>
                 <Image loading="lazy" src={Recruting} alt="" />
               </div>
